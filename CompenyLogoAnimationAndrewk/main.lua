34da8e9f7f80b3430b3a71af79009baf804ec907
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2018-11-12 15:47:28 -0500

    updated

--- CompenyLogoAnimationAndrewk/main.lua
+++ CompenyLogoAnimationAndrewk/main.lua
@@ -1,7 +1,21 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title:Compeny Logo Animation
+-- Name: Andrew Jr
+-- Course: ICS2O/3C
+-- This program...
 
--- Your code here
+-- hide the status dar 
+display.setStatusBar(display.HiddenStatusBar)
+-- create a local variable
+local textObject
+
+
+-- sets  the background colour
+display.setDefault("background",20/255,210/255,10/255) 
+
+local Image = display.newImage("Image/AndrewLogo.png", 0, 0)
+
+
+
+
+-- sets the color of the text
+textObjectSignature = display.newText( "Jumping Animation", 600, 500, nil, 69 )
