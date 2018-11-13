22a495a570a130344b730ea98718ffaff5c84776
AndrewKuekam17 <43419144+AndrewKuekam17@users.noreply.github.com>
2018-11-13 15:53:20 -0500

    Updated

--- CompenyLogoAnimationAndrewk/main.lua
+++ CompenyLogoAnimationAndrewk/main.lua
@@ -3,19 +3,52 @@
 -- Course: ICS2O/3C
 -- This program...
 
+----------------------------------------------------------------------------------------
+--SOUNDS 
+-----------------------------------------------------------------------------------------
+
+local jazzyfrenchySound = audio.loadSound("Sound/jazzyfrenchy.mp3")--Setting a variable to an mp3 file
+local jazzyfrenchyChannel 
+local jazzyfrenchySoundChannel = audio.play(jazzyfrenchySound)
+
 -- hide the status dar 
 display.setStatusBar(display.HiddenStatusBar)
--- create a local variable
-local textObject
+
+-- glodal varibles
+scrollSpeed = 5
 
 
--- sets  the background colour
-display.setDefault("background",20/255,210/255,10/255) 
+-- background image with width and height
+local backgroundImage = display.newImageRect("Images/background.png", 234/255, 170/255, 83/255)
 
-local Image = display.newImage("Image/AndrewLogo.png", 0, 0)
+-- character image with width and height 
+local AndrewLogo = display.newImageRect("Image/AndrewLogo.png", 800, 800)
 
 
+--set the image to be transparent
+AndrewLogo.alpha = 0
 
+AndrewLogo.alpha = 1
 
--- sets the color of the text
-textObjectSignature = display.newText( "Jumping Animation", 600, 500, nil, 69 )
+--set the initial x and y position of AndrewLogo
+AndrewLogo.x = 0
+AndrewLogo.y = display.contentHeight/3
+
+--set the initial x and y position of AndrewLogo
+AndrewLogo.x = 0
+AndrewLogo.y = 400
+
+-- Function: AndrewLogo
+-- Input this function accepts an event listener
+-- Output: none
+-- Description:This function adds the scroll speed to the x-value of the ship
+local function MoveAndrewLogo(event) 
+    -- add the scroll speed to the x-value of the ship
+    AndrewLogo.x = AndrewLogo.x + scrollSpeed  
+    AndrewLogo.alpha = AndrewLogo.alpha - 0.03
+    -- change the transparency of the ship every time it moves so that it fades out
+    AndrewLogo.alpha = AndrewLogo.alpha + 0.01
+end
+
+-- MoveShip will be called over and over again
+Runtime:addEventListener("enterFrame", MoveAndrewLogo)    
